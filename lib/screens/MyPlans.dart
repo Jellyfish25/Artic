5e5f8fd6b62ffba9b00d8f1dd7a7a1e7e0233d13
Jellyfish25 <mitkodimitrov50@gmail.com>
2022-08-15 16:20:54 -0700

    updated overview & finished MyPlans page

--- lib/screens/MyPlans.dart
+++ lib/screens/MyPlans.dart
@@ -24,7 +24,8 @@ class _MyPlansState extends State<MyPlans> {
     '9',
   ];
   int planIndex = -1;
-  String planName = '';
+  int favoriteIndex = -1;
+  String favoritePlan = '';
 
   @override
   Widget build(BuildContext context) {
@@ -34,42 +35,74 @@ class _MyPlansState extends State<MyPlans> {
       appBar: const KAppBar(title: "My Plans"),
       body: Column(
         children: [
-          Expanded(
+          const SizedBox(height: 20),
+          Container(
+            width: MediaQuery.of(context).size.width - 20,
+            height: MediaQuery.of(context).size.width + 40,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              color: const Color(0xFFB0E8FA),
+              border: Border.all(
+                width: 3,
+                color: const Color(0xFF2194D2),
+              ),
+            ),
             child: ListView.builder(
               itemCount: plansLength,
               itemBuilder: (context, index) {
                 return Container(
                   color: Colors.transparent,
-                  child: Transform.translate(
-                    // adjust offset to change the list tile left to right spacing
-                    offset: const Offset(25, 0),
-                    child: ListTile(
-                      onTap: () {},
-                      onLongPress: () {
+
+                  //******************* Single tap to favorite (single star)
+                  // child: Transform.translate(
+                  //   // adjust offset to change the list tile left to right spacing
+                  //   offset: const Offset(25, 0),
+                  //   child: ListTile(
+                  //     onTap: () {
+                  //       setState(() {
+                  //         planIndex = index;
+                  //         favoritePlan = plans[index];
+                  //       });
+                  //       print('Selected Item index at: $favoritePlan');
+                  //     },
+                  //     leading: Visibility(
+                  //       visible: planIndex == index,
+                  //       child: const Icon(
+                  //         Icons.star,
+                  //         size: 40,
+                  //         color: Colors.black,
+                  //       ),
+                  //     ),
+
+                  //***************** One tap to favorite, one tap to edit
+                  child: ListTile(
+                    onTap: () {},
+                    leading: IconButton(
+                      color: Colors.black,
+                      onPressed: () {
                         setState(() {
-                          planIndex = index;
-                          planName = plans[index];
+                          favoriteIndex = index;
+                          favoritePlan = plans[index];
+                          print('favorite plan: $favoritePlan');
                         });
-                        print('Selected Item index at: $planName');
                       },
-                      leading: Visibility(
-                        visible: planIndex == index,
-                        child: const Icon(
-                          Icons.star,
-                          size: 40,
-                          color: Colors.black,
-                        ),
-                      ),
-                      title: Transform.translate(
-                        // adjust offset to change the icon to text spacing
-                        offset: const Offset(0, 0),
-                        child: Text(
-                          plans[index],
-                          style: GoogleFonts.roboto(
-                            fontWeight: FontWeight.w700,
-                            fontStyle: FontStyle.normal,
-                            fontSize: 24,
-                          ),
+                      icon: favoriteIndex == index
+                          ? const Icon(Icons.star,
+                              color: Colors.yellow, size: 30)
+                          : const Icon(Icons.star_border,
+                              //either make it gray, light gray, or white
+                              color: Color(0xD9EEEEEE),
+                              size: 30),
+                    ),
+                    title: Transform.translate(
+                      // adjust offset to change the icon to text spacing
+                      offset: const Offset(0, 0),
+                      child: Text(
+                        plans[index],
+                        style: GoogleFonts.roboto(
+                          fontWeight: FontWeight.w700,
+                          fontStyle: FontStyle.normal,
+                          fontSize: 24,
                         ),
                       ),
                     ),
@@ -80,28 +113,31 @@ class _MyPlansState extends State<MyPlans> {
           ),
           Align(
             alignment: Alignment.bottomCenter,
-            child: MaterialButton(
-              minWidth: 339.0,
-              height: 85.0,
-              color: const Color(0xff1375CF),
-              shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(22.0)),
-              onPressed: () {
-                print("you pressed me");
-              },
-              child: Text(
-                'CREATE PLAN',
-                style: GoogleFonts.roboto(
-                  fontWeight: FontWeight.w700,
-                  fontStyle: FontStyle.normal,
-                  fontSize: 30,
-                  color: Colors.white,
+            child: Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: MaterialButton(
+                minWidth: MediaQuery.of(context).size.width / 5,
+                height: MediaQuery.of(context).size.height / 10,
+                color: const Color(0xff1375CF),
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(22.0)),
+                onPressed: () {
+                  print("you pressed me");
+                },
+                child: Text(
+                  'CREATE PLAN',
+                  style: GoogleFonts.roboto(
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
                 ),
               ),
             ),
           ),
           const SizedBox(
-            height: 20,
+            height: 30,
           )
         ],
       ),
--- lib/screens/Overview.dart
+++ lib/screens/Overview.dart
@@ -1,54 +1,150 @@
+import 'package:artic/screens/WelcomeScreen.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-
+import 'package:liquid_progress_indicator/liquid_progress_indicator.dart';
 import '../constants.dart';
 
-class Overview extends StatelessWidget {
-  static const String id = 'homepage';
+class Overview extends StatefulWidget {
+  static const String id = 'overview';
+
+  @override
+  State<Overview> createState() => _OverviewState();
+}
+
+class _OverviewState extends State<Overview> {
+  List<String> courseList = [
+    'Engr 10',
+    'Intro to Java',
+    'Math 101A',
+    'Physics',
+  ];
+  final int barStatus = 50;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: const KAppBar(title: 'Overview'),
-      body: Center(
-        child: Column(
-          children: [
-            const SizedBox(height: 20),
-            Text(
-              'You\'re This Close to Graduating!',
-              style: GoogleFonts.roboto(
-                height: 0.7,
-                fontWeight: FontWeight.w400,
-                fontStyle: FontStyle.normal,
-                fontSize: 24,
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(height: 20),
+              Text(
+                'You\'re This Close to Graduating!',
+                style: GoogleFonts.roboto(
+                  height: 0.7,
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 24,
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Image.asset('images/gradBar.png'),
-            const SizedBox(height: 50),
-            Text(
-              '3/4 Courses Complete for degree:',
-              style: GoogleFonts.roboto(
-                height: 0.7,
-                fontWeight: FontWeight.w400,
-                fontStyle: FontStyle.normal,
-                decoration: TextDecoration.underline,
-                fontSize: 20,
+              const SizedBox(height: 20),
+              //Image.asset('images/gradBar.png'),
+              //*********************** progress bar
+              SizedBox(
+                height: 40,
+                width: 400,
+                child: LiquidLinearProgressIndicator(
+                  value: (barStatus / 100).toDouble(),
+                  valueColor: const AlwaysStoppedAnimation(Color(0xFF03BEFC)),
+                  backgroundColor: Colors.white,
+                  borderColor: const Color(0xFF022F3B),
+                  borderWidth: 3.0,
+                  borderRadius: 12.0,
+                  direction: Axis.horizontal,
+                  center: Text(
+                    '$barStatus%',
+                    style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w500,
+                        fontStyle: FontStyle.normal,
+                        fontSize: 24,
+                        color: const Color(0xFFFA14AD)),
+                  ),
+                ),
               ),
-            ),
-            const SizedBox(height: 30),
-            Text(
-              'CMPE - SJSU',
-              style: GoogleFonts.roboto(
-                height: 0.7,
-                fontWeight: FontWeight.w400,
-                fontStyle: FontStyle.normal,
-                fontSize: 40,
+              const SizedBox(height: 50),
+              Text(
+                '3/4 Courses Complete for degree:',
+                style: GoogleFonts.roboto(
+                  height: 0.7,
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  decoration: TextDecoration.underline,
+                  fontSize: 20,
+                ),
               ),
-            ),
-            const SizedBox(height: 20),
-            Image.asset('images/inProgressCourses.png'),
-          ],
+              const SizedBox(height: 30),
+              Text(
+                'CMPE - SJSU',
+                style: GoogleFonts.roboto(
+                  height: 0.7,
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                  fontSize: 40,
+                ),
+              ),
+              const SizedBox(height: 50),
+
+              //Image.asset('images/inProgressCourses.png'),
+              // ******************************************* Course Display List
+              Container(
+                // adjust container width/height to fit box better
+                width: 380,
+                height: 300,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: const Color(0xFFB0E8FA),
+                  border: Border.all(
+                    width: 3,
+                    color: const Color(0xFF2194D2),
+                  ),
+                ),
+                child: Column(
+                  children: [
+                    const SizedBox(height: 10),
+                    Text(
+                      'Courses Currently being Taken',
+                      style: GoogleFonts.roboto(
+                        fontWeight: FontWeight.w700,
+                        fontStyle: FontStyle.normal,
+                        color: const Color(0xFF1375CF),
+                        fontSize: 20,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    const Divider(
+                      thickness: 5,
+                      color: Color(0xFFC8F1FE),
+                    ),
+                    const SizedBox(height: 5),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: courseList.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Transform.translate(
+                            // adjust offset to change the list tile left to right spacing
+                            offset: const Offset(60, 0),
+                            child: ListTile(
+                              dense: true,
+                              visualDensity: const VisualDensity(vertical: -3),
+                              title: Text(
+                                '${index + 1}. ${courseList[index]}',
+                                style: GoogleFonts.roboto(
+                                  fontWeight: FontWeight.w700,
+                                  fontStyle: FontStyle.normal,
+                                  fontSize: 20,
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
       drawer: kNavBar,
